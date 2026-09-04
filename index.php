eff2224f8eb6fc5cd560765977bf77a1ff6c687c
Richard Dzikia <richarddzikia@gmail.com>
2026-09-04 09:43:17 +0300

    add Maths examples

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
 $test = 10;
-$test = 3.1;
-$test = "Hello";
-$test = 'õpilane';
-$test = true;
-$test = false;
-$test = null;
-$test = [1, true, 'sus'];
-$test = new stdClass();
-$test->name = 'Richard';
+$test = 10 + 10;
+$test = 5 % 2;
+$test = 2 ** 3;// 2 to the power of 3
+$test = $test + 10;
+$test += 10;
+$test++;
+$test--;
+++$test;
+--$test;
+$a = 1;
+$b = ++$a;
+var_dump($b, $a);
+$test = 10;
+$test = sin($test);
 var_dump($test);
+#
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
