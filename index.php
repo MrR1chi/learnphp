24cbf341c4782c0eeb26faea8abde271b2ff5310
Richard Dzikia <richarddzikia@gmail.com>
2026-09-04 09:28:31 +0300

    add types examples

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<h1>
 <?php
-echo "Hello, World!";
-?>
-</h1>
+$test = 10;
+$test = 3.1;
+$test = "Hello";
+$test = 'õpilane';
+$test = true;
+$test = false;
+$test = null;
+$test = [1, true, 'sus'];
+$test = new stdClass();
+$test->name = 'Richard';
+var_dump($test);
