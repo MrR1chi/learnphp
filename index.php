b8f71edab938e2fa38056e3fe6f5b8074099b011
Richard Dzikia <richarddzikia@gmail.com>
2026-09-16 11:45:36 +0300

    add visibility example

--- index.php
+++ index.php
@@ -1,21 +1,46 @@
 <?php
 
 class Box {
-    public $width;
-    public $height;
-    public $length;
+    public int $width;
+    private int $height;
+    protected int $length;
 
     public function volume(){
         return $this->width * $this->height * $this->length;
     }
+
+    public function setHeight(int $height){
+        if($height < 0) {
+            $this->height = 0;
+        } else {
+            $this->height = $height;
+        }
+    }
+    public function getHeight(){
+        return $this->height;
+    }
+
+    public function test1(){
+        $this->length = 10;
+        var_dump($this->length);
+    }
 }
 
 class MetalBox extends Box {
-    public $weightPerUnit;
+    public int $weightPerUnit;
     public function mass(){
         return $this->volume() * $this->weightPerUnit;
     }
+
+    public function test2(){
+        $this->height = 10;
+        var_dump($this->height);
+    }
 }
+
+$box1 = new Box();
+$box1->width = 10;
+var_dump($box1->height);
 
 $metal1 = new MetalBox();
 var_dump($metal1);
