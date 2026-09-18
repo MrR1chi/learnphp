9d6d0d1c677d33b9e17238263469bd8b40080110
Richard Dzikia <richarddzikia@gmail.com>
2026-09-18 13:26:17 +0300

    add magic methods example

--- index.php
+++ index.php
@@ -1,62 +1,58 @@
 <?php
 
 class Box {
-    use HasColor, HasSmell;
-    public int $width;
-    private int $height;
-    protected int $length;
+
+
+    public function __construct(public int $width, private int $height, protected int $length){
+        var_dump("Box was created");
+    }
 
     public function volume(){
         return $this->width * $this->height * $this->length;
     }
 
-    public function setHeight(int $height){
-        if($height < 0) {
-            $this->height = 0;
-        } else {
-            $this->height = $height;
-        }
-    }
-    public function getHeight(){
-        return $this->height;
+    public function __set($name, $value) {
+        var_dump("Setting '$name' to '$value'");
     }
 
-    public function test1(){
-        $this->length = 10;
-        var_dump($this->length);
+    public function __get($name) {
+        var_dump("Getting '$name'");
+        return 'some value';
     }
+
+    public function __call($name, $arguments) {
+        var_dump($name, $arguments);
+        
+    }
+
+    public function __invoke($arguments) {
+        var_dump($arguments);
+    }
+
+    public function __toString(){
+        return "Im a box";
+    }
+
+    public function __destruct(){
+        var_dump("Box was destroyed");
+    }
+
 }
 
-class MetalBox extends Box {
-    public int $weightPerUnit;
-    public function mass(){
-        return $this->volume() * $this->weightPerUnit;
-    }
+function test() {
+    $box2 = new Box(1, 2, 3);
+}
+test();
 
-    public function test2(){
-        $this->height = 10;
-        var_dump($this->height);
-    }
+for($i=0; $i<10; $i++){
+    $box2 =new Box(1, 2, 3);
 }
 
-trait HasColor {
-    public $color;
-    public function setColor($color){
-        $this->color = $color;
-    }
-}
-
-trait HasSmell {
-    public $smell;
-    public function setSmell($smell){
-        $this->smell = $smell;
-    }
-}
-
-$box1 = new Box();
-$box1->width = 10;
-$box1->setHeight(5);
-var_dump($box1->getHeight());
-
-$metal1 = new MetalBox();
-var_dump($metal1);
+$box = new Box(1, 2, 3);
+$box->hello = 'yolo';
+var_dump($box->yolo);
+$box->cool(1, 'lol', 42);
+$box(1, 'yes');
+$box= 1;
+var_dump($box);
+echo $box;
